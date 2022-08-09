0b9c2c2ac18e8588e29cc26b46ba5d66acbd3f78
HRITIKYADAV <100505402+REDSKULL19@users.noreply.github.com>
2022-08-10 01:28:41 +0530

    Add files via upload

---
+++ que4.java
@@ -0,0 +1,20 @@
+// package assignment1;
+
+public class que4 {
+    public static void main(String args[]) {
+        int n =13;
+        for (int i=0; i<n ; i++){
+              for(int j=0; j<n; j++){
+  
+                  if( j==0  && j >= (n-1)/2|| i==n-1 || j==n-1 && i>= n/2  || j<= i -(n-1)/2 ||  j >= n-1 + n/2 -i ){
+                      System.out.print("* ");
+                  }
+                  else{
+                      System.out.print("  ");
+                  }          
+              }
+              System.out.println();
+        }
+      }
+    }
+
